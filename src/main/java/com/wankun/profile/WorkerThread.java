71039466c1b04730b61a7b49dfe10051a9c96dc7
wankunde <wankunde@163.com>
2020-05-18 21:23:42 +0800

    add test class

--- src/main/java/com/wankun/profile/WorkerThread.java
+++ src/main/java/com/wankun/profile/WorkerThread.java
@@ -10,8 +10,8 @@ public class WorkerThread {
 
   public static void main(String[] args) throws InterruptedException {
     int count = 0;
-    for (int i = 0; i < 100000; i++) {
-      Thread.sleep(10);
+    for (int i = 0; i < 10000; i++) {
+//      Thread.sleep(10);
       count++;
       int r = new Random().nextInt(100);
       System.out.println(r);
